d5f760666f6f71cf94df9e6575d045f8f7363468
DanylenkoTetiana <110232913+DanylenkoTetiana@users.noreply.github.com>
2022-10-14 13:48:22 +0000

    Deploying to gh-pages from @ DanylenkoTetiana/goit-js-hw-10@d05fc1dd5b141ce0c73e537fe88fe081ad5ffd1b 🚀

---
+++ index.20f9667e.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var o=document.querySelector("#search-box");console.log(o),o.style.borderColor="blue",o.style.margin="15px",console.log(document.body),document.body.style.background="url(./image/photo.jpg)",o.style.backgroundImage="url(/image/background.jpg)"}();
+//# sourceMappingURL=index.20f9667e.js.map
