c6282cbc2b33c8282b18bcb2c4152fec678fb5fd
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-14 16:56:08 +0300

    зміна адреси фото

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ console.log(inputEl);
 inputEl.style.borderColor = 'blue';
 inputEl.style.margin = '15px';
 console.log(document.body);
-document.body.style.background = 'url(./image/photo.jpg)';
+document.body.style.backgroundImage = 'url(./image/photo.jpg)';
 inputEl.style.backgroundImage = 'url(/image/photo.jpg)';
 // document.body.style.background =
 //   '#fff url(https://image.ibb.co/mjdbZ5/bg5.jpg) no-repeat center';
