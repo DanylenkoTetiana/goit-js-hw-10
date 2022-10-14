d05fc1dd5b141ce0c73e537fe88fe081ad5ffd1b
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-14 16:47:57 +0300

    додано мінімальне оформлення

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+'use strict';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+console.log(inputEl);
+inputEl.style.borderColor = 'blue';
+inputEl.style.margin = '15px';
+console.log(document.body);
+document.body.style.background = 'url(./image/photo.jpg)';
+inputEl.style.backgroundImage = 'url(/image/background.jpg)';
+// document.body.style.background =
+//   '#fff url(https://image.ibb.co/mjdbZ5/bg5.jpg) no-repeat center';
