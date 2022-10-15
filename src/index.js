6c8b4bb1a0d0d8b22d44dd4f1128e17078d88db6
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-15 21:39:54 +0300

    додає нове фото бекграунду

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 console.log(inputEl);
-inputEl.style.borderColor = 'blue';
+inputEl.style.border = '#696969 solid 3px';
 inputEl.style.margin = '15px';
 console.log(document.body);
-document.body.style.backgroundImage = 'url(../image/photo.jpg)';
-inputEl.style.backgroundImage = 'url(/image/photo.jpg)';
-// document.body.style.background =
-//   '#fff url(https://image.ibb.co/mjdbZ5/bg5.jpg) no-repeat center';
+document.body.style.backgroundImage =
+  'url(https://st.depositphotos.com/2016341/2010/i/450/depositphotos_20105595-stock-photo-white-grunge-brick-wall-background.jpg)';
