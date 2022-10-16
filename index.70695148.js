c56efdddc9949a9d18c3a8c583489a5f3845c6ed
DanylenkoTetiana <110232913+DanylenkoTetiana@users.noreply.github.com>
2022-10-16 12:38:57 +0000

    Deploying to gh-pages from @ DanylenkoTetiana/goit-js-hw-10@82512b2d2b5e817524e068f3dbd359b903ed70af 🚀

--- index.70695148.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("#search-box");console.log(o),o.style.border="#696969 solid 3px",o.style.margin="15px",console.log(document.body),document.body.style.backgroundImage="url(https://st.depositphotos.com/2016341/2010/i/450/depositphotos_20105595-stock-photo-white-grunge-brick-wall-background.jpg)";
-//# sourceMappingURL=index.70695148.js.map
--- index.b7b219c2.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var o=document.querySelector("#search-box");console.log(o),o.style.border="#696969 solid 3px",o.style.margin="15px",console.log(document.body),document.body.style.backgroundImage="url(https://st.depositphotos.com/2016341/2010/i/450/depositphotos_20105595-stock-photo-white-grunge-brick-wall-background.jpg)"}();
-//# sourceMappingURL=index.b7b219c2.js.map
