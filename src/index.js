82512b2d2b5e817524e068f3dbd359b903ed70af
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-16 15:38:32 +0300

    змінює імпорт функції

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
